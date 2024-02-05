9e4a6c194d9f67667b3da3a7a208f21fcd5ca8af
Lucaturin2003 <159005233+Lucaturin2003@users.noreply.github.com>
2024-02-05 17:48:45 +0100

    Add files via upload

---
+++ Fields/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace Fields
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public readonly List<Order> Orders = new List<Order>();
+
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+        public Customer(int id, string name): this(id)
+        {
+            this.Name = name;   
+        }
+
+        public void Promote()
+        {
+
+        }
+
+    }
+    public class Order
+    {
+
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+
+            Console.WriteLine(customer.Orders.Count);
+        }
+    }
+}
